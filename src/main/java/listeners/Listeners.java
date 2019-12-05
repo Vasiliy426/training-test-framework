06a6fd2efe1068eab508a59a813f185d5532d768
vasiliy.dashkevich <Vasiq426@gmail.com>
2019-12-05 13:20:02 +0300

    Some changes:
    in listeners, reporter (extent) and logger (log4j).
    Anyways, haven't finished that yet.

--- src/main/java/listeners/Listeners.java
+++ src/main/java/listeners/Listeners.java
@@ -8,8 +8,6 @@ import org.testng.ITestResult;
 import org.testng.Reporter;
 
 public class Listeners implements ITestListener {
-    private long suiteExecClearTime = 0L;
-
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
@@ -19,19 +17,16 @@ public class Listeners implements ITestListener {
     @Override
     public void onTestSuccess(ITestResult result) {
         extentTest.log(LogStatus.PASS, "Test passed");
-        suiteExecClearTime += result.getEndMillis() - result.getStartMillis();
     }
 
     @Override
     public void onTestFailure(ITestResult result) {
         Reporter.getExtentTest.log(LogStatus.FAIL, "Test failed >>> "+ result.getThrowable());
-        suiteExecClearTime += result.getEndMillis() - result.getStartMillis();
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
         Reporter.getExtentTest.log(LogStatus.SKIP, "Test skipped >>>" + result.getThrowable());
-        suiteExecClearTime += result.getEndMillis() - result.getStartMillis();
     }
 
     @Override
@@ -41,15 +36,11 @@ public class Listeners implements ITestListener {
 
     @Override
     public void onStart(ITestContext context) {
-        int testMethodsCount = context.getAllTestMethods().length;
+        // stub
     }
 
     @Override
     public void onFinish(ITestContext context) {
-        float suiteExecClearTimeSecs = convertMillisToDecimalSeconds(suiteExecClearTime);
-    }
-
-    private float convertMillisToDecimalSeconds(long millis) {
-        return millis / 1000.0f;
+        Reporter.getExtentTest.log(LogStatus.INFO, "Test suite finished.");
     }
 }
---
+++ src/main/java/logger/LoggerTool.java
@@ -0,0 +1,31 @@
+package logger;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+import static util.Constants.LOG4J_CONFIG_PATH;
+
+public class LoggerTool {
+    private static final Logger LOGGER = Logger.getLogger(LoggerTool.class); //logger name
+
+    public static void logSetUp() {
+        DOMConfigurator.configure(LOG4J_CONFIG_PATH);
+    }
+
+    public static void logInfo(String message) {
+        LOGGER.info("Info: " + message);
+    }
+
+    public static void logBoldInfo(String message) {
+        LOGGER.info("RP_MESSAGE#FILE#" + "#" + "!!!MARKDOWN_MODE!!!" + "***" + message + "***");
+    }
+
+    public static void logWarning(String message) {
+        LOGGER.warn("Warning: " + message);
+    }
+
+    public static void logError(String message) {
+        LOGGER.error("ERROR: " + message);
+    }
+
+}
